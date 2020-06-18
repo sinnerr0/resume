93a438825474db1a48bfdfb8a9431bbff49c6d0c
KyongSik Choi <ks.choi@alcherainc.com>
2020-06-18 20:47:24 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img src="/profile.jpg" />
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: '[Resume] Kyongsik.C',
+      meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+      link: [{ rel: 'favicon', href: 'favicon.ico' }],
+    }
+  },
+}
+</script>
